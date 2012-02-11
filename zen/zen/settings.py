0ee9492b57bf45c3e6bf9799af85fc1e2cc33533
Craig Slusher <cslush@gmail.com>
2012-02-11 13:24:08 -0800

    renamed

--- zen/zen/settings.py
+++ zen/zen/settings.py
@@ -1,4 +1,4 @@
-# Django settings for bootstrap project.
+# Django settings for zen project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -81,7 +81,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '-a5+hc0ok5-a2p5kzsjy%py0skou@ax0^7h!4)zcq*zqc%afs#'
+SECRET_KEY = '1d$_mu4&amp;a6)!&amp;$-99aer(cyp88ebk-9f2j4o2%8ko-!6up$gd('
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -100,10 +100,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'bootstrap.urls'
+ROOT_URLCONF = 'zen.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'bootstrap.wsgi.application'
+WSGI_APPLICATION = 'zen.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
--- zen/zen/urls.py
+++ zen/zen/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'bootstrap.views.home', name='home'),
-    # url(r'^bootstrap/', include('bootstrap.foo.urls')),
+    # url(r'^$', 'zen.views.home', name='home'),
+    # url(r'^zen/', include('zen.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
