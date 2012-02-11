dbcad95db7071284ee80f07d27b382a39e16f862
Alec Koumjian <akoumjian@gmail.com>
2012-02-11 13:51:16 -0800

    adding foundation app as main index

--- zen/zen/apps/foundation/urls.py
+++ zen/zen/apps/foundation/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', include('apps.foundation.urls')),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
     # Examples:
     # url(r'^$', 'zen.views.home', name='home'),
     # url(r'^zen/', include('zen.foo.urls')),
